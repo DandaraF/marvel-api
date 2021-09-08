33aedc31ccdf861767e122a62d4b751ca7fc528c
Dandara Silva <dandara-chr@hotmail.com>
2021-09-08 19:36:44 -0300

    create page series

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,23 +2,6 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100%;
-  gap: 20px;
 `;
 
-export const Content = styled.div`
-  min-width: 350px;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  gap: 8px;
-  flex-flow: row wrap;
-`;
-
-export const Title = styled.h1``;
-
-export const Text = styled.p``;
-
-export const Button = styled.button`
-  width: 80px;
-  height: 30px;
-`;
+export const Content = styled.div``;
